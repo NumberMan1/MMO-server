f404018cc49b6de20c570b590b36da9169c5b6a6
NumberMan <2100600995@qq.com>
2024-03-28 22:40:10 +0800

    fix: 使用sync.Map代替map

--- service/net_service.go
+++ service/net_service.go
@@ -6,6 +6,7 @@ import (
 	"github.com/NumberMan1/common/summer/network"
 	"github.com/NumberMan1/common/summer/network/core"
 	"github.com/NumberMan1/common/summer/protocol/gen/proto"
+	"sync"
 	"time"
 )
 
@@ -13,7 +14,8 @@ import (
 type NetService struct {
 	tcpServer *core.TcpServer
 	//记录conn最后一次心跳包的时间
-	heartBeatPairs map[network.Connection]time.Time
+	//heartBeatPairs map[network.Connection]time.Time
+	heartBeatPairs *sync.Map
 	heartTicker    *time.Ticker
 	cancel         chan struct{}
 }
@@ -21,8 +23,9 @@ type NetService struct {
 func NewNetService() *NetService {
 	server, _ := core.NewTcpServer("127.0.0.1:32510")
 	n := &NetService{
-		tcpServer:      server,
-		heartBeatPairs: map[network.Connection]time.Time{},
+		tcpServer: server,
+		//heartBeatPairs: map[network.Connection]time.Time{},
+		heartBeatPairs: &sync.Map{},
 		heartTicker:    time.NewTicker(5 * time.Second),
 		cancel:         make(chan struct{}, 1),
 	}
@@ -52,7 +55,8 @@ func (n *NetService) Stop() {
 
 // 收到心跳包
 func (n *NetService) heartBeatRequest(msg network.Msg) {
-	n.heartBeatPairs[msg.Sender] = time.Now()
+	//n.heartBeatPairs[msg.Sender] = time.Now()
+	n.heartBeatPairs.Store(msg.Sender, time.Now())
 	p := &proto.HeartBeatResponse{}
 	msg.Sender.Send(p)
 }
@@ -62,14 +66,25 @@ func (n *NetService) timerCallback() {
 		select {
 		case <-n.heartTicker.C:
 			now := time.Now()
-			for conn, tp := range n.heartBeatPairs {
+			//for conn, tp := range n.heartBeatPairs {
+			//	cha := now.Sub(tp)
+			//	//关闭超时的客户端连接
+			//	if cha.Seconds() > (10 * time.Second).Seconds() {
+			//		conn.Close()
+			//		delete(n.heartBeatPairs, conn)
+			//	}
+			//}
+			n.heartBeatPairs.Range(func(k, v any) bool {
+				tp := v.(time.Time)
+				conn := k.(network.Connection)
 				cha := now.Sub(tp)
 				//关闭超时的客户端连接
 				if cha.Seconds() > (10 * time.Second).Seconds() {
 					conn.Close()
-					delete(n.heartBeatPairs, conn)
+					n.heartBeatPairs.Delete(conn)
 				}
-			}
+				return true
+			})
 		case <-n.cancel:
 			return
 		}
@@ -79,12 +94,14 @@ func (n *NetService) timerCallback() {
 // 当客户端接入
 func (n *NetService) onClientConnected(conn network.Connection) {
 	logger.SLCInfo("客户端接入")
-	n.heartBeatPairs[conn] = time.Now()
+	//n.heartBeatPairs[conn] = time.Now()
+	n.heartBeatPairs.Store(conn, time.Now())
 	conn.Set("Session", core2.NewSession())
 }
 
 func (n *NetService) onDisconnected(conn network.Connection) {
-	delete(n.heartBeatPairs, conn)
+	//delete(n.heartBeatPairs, conn)
+	n.heartBeatPairs.Delete(conn)
 	logger.SLCInfo("连接断开:%v", conn.Socket().RemoteAddr().String())
 	character := conn.Get("Session").(*core2.Session).Character
 	if character != nil {
